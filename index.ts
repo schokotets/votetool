1d5467598c6f803e76bf50d6b0c615b050abb7e6
schokotets <moritzleni@gmail.com>
2020-10-11 16:20:54 +0200

    compatibilize with koa, form parsing, mustache template

--- index.ts
+++ index.ts
@@ -1,43 +1,45 @@
+const fs = require("fs")
+
 const Koa = require("koa")
-const app = new Koa();
+const app = new Koa()
 
-await db.connect()
-await db.initialize()
+app.use(require('koa-static')("./static", { maxage: 86400000 /*1 day*/ }))
+app.use(require('koa-bodyparser')())
 
-app.use(require('koa-static')("./static", { maxage: 86400000 /*1 day*/ }));
-const s = app.listen(8083);
-console.log("listening on :8083");
+const Mustache = require("mustache")
+
+const db = require("./db")
+
+
+
+db.connect().then(db.initialize).then(() => {
+  app.listen(8083)
+  console.log("listening on :8083")
+})
 
 app.use(async ctx => {
   if(ctx.url == "/vote") {
     let data = {options: await db.getVotes()}
-    let body = await renderFile("./vote.html", data)
-    req.respond({ body: body });
+    ctx.body = await Mustache.render(fs.readFileSync(__dirname + "vote.html").toString(), data)
 
   } else if(ctx.url == "/submit") {
-    let headers = new Headers()
-
-    let cookies = getCookies(req)
-    if ("voted-abimotto" in cookies) {
-      req.respond({ status: 401, body: "Bereits abgestimmt.\nSchau dir die Ergebnisse an." })
-      continue
+    if (ctx.cookies.get("voted-abimotto")) {
+      ctx.throw(401, "Bereits abgestimmt.\nSchau dir die Ergebnisse an.")
+      return
     }
 
-    let hashedip
-    if(req.conn.remoteAddr.transport == "tcp") {
-      const ip = req.conn.remoteAddr.hostname
-      hashedip = hash.digest(encode(ip)).hex()
-      if (await db.hasVoted(hashedip)) {
-        req.respond({ status: 401, body: "Bereits abgestimmt.\nSchau dir die Ergebnisse an." })
-        continue
-      }
+    const hashedip = hash.digest(encode(ctx.ip)).hex()
+    if (await db.hasVoted(hashedip)) {
+      ctx.throw(401, "Bereits abgestimmt.\nSchau dir die Ergebnisse an.")
+      return
     }
 
-    const form = await multiParserV2(req)
+    const form = await ctx.request.body
     if (form) {
+      console.log(form)
       if (!form.fields["consent"]) {
-        req.respond({ status: 406, body: "Zustimmung nicht gegeben.\nNavigiere bitte zurueck versuche es erneut." })
-        continue
+        ctx.throw(406, "Zustimmung nicht gegeben.\nNavigiere bitte zurueck versuche es erneut.")
+        return
       }
 
       let votes: string[] = []
@@ -49,42 +51,40 @@ app.use(async ctx => {
 
       let amount = votes.length
       if (amount == 0) {
-        req.respond({ status: 400, body: `Gar keine Optionen ausgewaehlt.\nNavigiere bitte zurueck und versuche es erneut.` })
-        continue
+        ctx.throw(400, `Gar keine Optionen ausgewaehlt.\nNavigiere bitte zurueck und versuche es erneut.`)
+        return
       }
       if (amount > 5) {
-        req.respond({ status: 400, body: `Zu viele Optionen ausgewaehlt (${amount} statt 5 erlaubte).\nNavigiere bitte zurueck und versuche es erneut.` })
-        continue
+        ctx.throw(400, `Zu viele Optionen ausgewaehlt (${amount} statt 5 erlaubte).\nNavigiere bitte zurueck und versuche es erneut.`)
+        return
       }
 
       let success = await db.castVotes(votes)
 
       if(!success) {
-        req.respond({ status: 500, body: "Interner Fehler beim Speichern der Stimmen.\nKontaktiere bitte den Seitenbetreiber.\nVersuche, zurück zu navigieren und es erneut zu versuchen." })
-        continue
+        ctx.throw(500, "Interner Fehler beim Speichern der Stimmen.\nKontaktiere bitte den Seitenbetreiber.\nVersuche, zurück zu navigieren und es erneut zu versuchen.")
+        return
       }
 
       console.log(`${new Date().toISOString()}: successful vote submission`)
 
       await db.noteVoted(hashedip)
-      headers.set("Set-Cookie", "voted-abimotto=true");
-      headers.set("Location", "/results")
-      req.respond({ status: 302, headers })
+      ctx.set("Set-Cookie", "voted-abimotto=true");
+      ctx.set("Location", "/results")
+      ctx.throw(302)
     } else {
-      req.respond({ status: 400, body: "Keine Formulardaten.\nNavigiere bitte zurueck und versuche es erneut." })
+      ctx.throw(400, "Keine Formulardaten.\nNavigiere bitte zurueck und versuche es erneut.")
     }
 
   } else if(ctx.url == "/results") {
     let data = {options: await db.getSortedVotes()}
-    let body = await renderFile("./results.html", data)
-    req.respond({ body: body });
+    ctx.body = await Mustache.render(fs.readFileSync(__dirname + "results.html").toString(), data)
 
   } else if(ctx.url == "/") {
-    let headers = new Headers()
-    headers.set("Location", "/results")
-    req.respond({ status: 302, headers })
+    ctx.set("Location", "/results")
+    ctx.throw(302)
 
   } else {
-    req.respond({ body: "404 Not found\n" });
+    ctx.throw(404, "404 Not found")
   }
 })

c53b25e473e2d37362c82e5e801cff4feb685089
schokotets <moritzleni@gmail.com>
2020-10-11 19:25:42 +0200

    merge from: move-to-node
    i needed to transition to node because there were issues with some
    unstable deno modules

--- index.ts
+++ index.ts
@@ -1,107 +1,93 @@
-import { multiParserV2 } from "https://deno.land/x/multiparser@v2.0.1/mod.ts"
+const fs = require("fs")
+const crypt = require("crypto")
 
-import { serve, ServerRequest } from "https://deno.land/std@0.61.0/http/server.ts";
-import { serveFile } from "https://deno.land/std@0.61.0/http/file_server.ts";
-import { getCookies } from "https://deno.land/std@0.61.0/http/cookie.ts";
-import { readLines } from "https://deno.land/std@0.61.0/io/bufio.ts";
-import { Hash, encode } from "https://deno.land/x/checksum@1.2.0/mod.ts";
+const Koa = require("koa")
+const app = new Koa()
 
-import { renderFile } from "https://deno.land/x/mustache/mod.ts";
+app.use(require("koa-static")("./static", { maxage: 86400000 /*1 day*/ }))
+app.use(require("multy")({}))
 
-import * as db from "./database.ts"
+const Mustache = require("mustache")
 
-const hash = new Hash("md5")
+const db = require("./database")
 
-async function serveF(req: ServerRequest, name: string) {
-  const content = await serveFile(req,  `${Deno.cwd()}/${name}`)
-  req.respond(content)
-}
 
-await db.connect()
-await db.initialize()
 
-const s = serve({ port: 8083 });
-console.log("listening on :8083");
-for await (const req of s) {
-  if(req.url == "/vote") {
+db.connect().then(db.initialize).then(() => {
+  app.listen(8083)
+  console.log("listening on :8083")
+})
+
+app.use(async ctx => {
+  if(ctx.url == "/vote") {
     let data = {options: await db.getVotes()}
-    let body = await renderFile("./vote.html", data)
-    req.respond({ body: body });
+    ctx.body = await Mustache.render(fs.readFileSync(__dirname + "/vote.html").toString(), data)
 
-  } else if(req.url == "/submit") {
-    let headers = new Headers()
-
-    let cookies = getCookies(req)
-    if ("voted-abimotto" in cookies) {
-      req.respond({ status: 401, body: "Bereits abgestimmt.\nSchau dir die Ergebnisse an." })
-      continue
+  } else if(ctx.url == "/submit") {
+    if (ctx.cookies.get("voted-abimotto")) {
+      ctx.throw(401, "Bereits abgestimmt.\nSchau dir die Ergebnisse an.")
+      return
     }
 
-    let hashedip
-    if(req.conn.remoteAddr.transport == "tcp") {
-      const ip = req.conn.remoteAddr.hostname
-      hashedip = hash.digest(encode(ip)).hex()
-      if (await db.hasVoted(hashedip)) {
-        req.respond({ status: 401, body: "Bereits abgestimmt.\nSchau dir die Ergebnisse an." })
-        continue
-      }
+    const hash = crypt.createHash("sha256")
+    hash.update(ctx.ip)
+    const hashedip = hash.digest("hex")
+    if (await db.hasVoted(hashedip)) {
+      ctx.throw(401, "Bereits abgestimmt.\nSchau dir die Ergebnisse an.")
+      return
     }
 
-    const form = await multiParserV2(req)
-    if (form) {
-      if (!form.fields["consent"]) {
-        req.respond({ status: 406, body: "Zustimmung nicht gegeben.\nNavigiere bitte zurueck versuche es erneut." })
-        continue
+    const formdata = ctx.request.body
+    if (formdata && Object.keys(formdata).length != 0) {
+      console.log(formdata)
+      if (!formdata["consent"]) {
+        ctx.throw(406, "Zustimmung nicht gegeben.\nNavigiere bitte zurueck versuche es erneut.")
+        return
       }
 
       let votes: string[] = []
-      Object.keys(form.fields).forEach(fieldkey => {
-        if (fieldkey != "consent" && form.fields[fieldkey].startsWith("on")) {
+      Object.keys(formdata).forEach(fieldkey => {
+        if (fieldkey != "consent" && formdata[fieldkey].startsWith("on")) {
           votes.push(fieldkey)
         }
       })
 
       let amount = votes.length
       if (amount == 0) {
-        req.respond({ status: 400, body: `Gar keine Optionen ausgewaehlt.\nNavigiere bitte zurueck und versuche es erneut.` })
-        continue
+        ctx.throw(400, `Gar keine Optionen ausgewaehlt.\nNavigiere bitte zurueck und versuche es erneut.`)
+        return
       }
       if (amount > 5) {
-        req.respond({ status: 400, body: `Zu viele Optionen ausgewaehlt (${amount} statt 5 erlaubte).\nNavigiere bitte zurueck und versuche es erneut.` })
-        continue
+        ctx.throw(400, `Zu viele Optionen ausgewaehlt (${amount} statt 5 erlaubte).\nNavigiere bitte zurueck und versuche es erneut.`)
+        return
       }
 
       let success = await db.castVotes(votes)
 
       if(!success) {
-        req.respond({ status: 500, body: "Interner Fehler beim Speichern der Stimmen.\nKontaktiere bitte den Seitenbetreiber.\nVersuche, zurück zu navigieren und es erneut zu versuchen." })
-        continue
+        ctx.throw(500, "Interner Fehler beim Speichern der Stimmen.\nKontaktiere bitte den Seitenbetreiber.\nVersuche, zurück zu navigieren und es erneut zu versuchen.")
+        return
       }
 
       console.log(`${new Date().toISOString()}: successful vote submission`)
 
       await db.noteVoted(hashedip)
-      headers.set("Set-Cookie", "voted-abimotto=true");
-      headers.set("Location", "/results")
-      req.respond({ status: 302, headers })
+      ctx.cookies.set("voted-abimotto","true");
+      ctx.status = 303
+      ctx.redirect("/results")
     } else {
-      req.respond({ status: 400, body: "Keine Formulardaten.\nNavigiere bitte zurueck und versuche es erneut." })
+      ctx.throw(400, "Keine Formulardaten.\nNavigiere bitte zurueck und versuche es erneut.")
     }
 
-  } else if(req.url == "/results") {
+  } else if(ctx.url == "/results") {
     let data = {options: await db.getSortedVotes()}
-    let body = await renderFile("./results.html", data)
-    req.respond({ body: body });
+    ctx.body = await Mustache.render(fs.readFileSync(__dirname + "/results.html").toString(), data)
 
-  } else if(req.url == "/") {
-    let headers = new Headers()
-    headers.set("Location", "/results")
-    req.respond({ status: 302, headers })
-
-  } else if(req.url == "/style.css") {
-    serveF(req, "style.css")
+  } else if(ctx.url == "/") {
+    ctx.status = 303
+    ctx.redirect("/results")
 
   } else {
-    req.respond({ body: "404 Not found\n" });
+    ctx.throw(404, "404 Not found")
   }
-}
+})
